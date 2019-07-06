481dbe50c1a3dd9e1c3dd10da4a9e8c3406a6904
Yasir Durrani <yudurrani@gmail.com>
2019-07-06 15:03:22 -0400

    Creating Tasks in Mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,74 +6,80 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name:{
-        type: String,
-        required: true,
-        trim: true,
-    },
-    email:{
-        type:String,
-        required: true,
-        trim: true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default:0,
-        validate(value){
-            if(value <0){
-                throw new Error('Age must be a positive number')
-            }
-        }
+// const User = mongoose.model('User', {
+//     name:{
+//         type: String,
+//         required: true,
+//         trim: true,
+//     },
+//     email:{
+//         type:String,
+//         required: true,
+//         trim: true,
+//         lowercase:true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Email is invalid')
+//             }
+//         }
+//     },
+//     password: {
+//         type:String,
+//         requried:true,
+//         trim: true,
+//         minlength:7,
+//         validate(value){
+//             if(value.toLowerCase().includes('password')){
+//                 throw new Error('Password cannot contain "password"' )
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default:0,
+//         validate(value){
+//             if(value <0){
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
         
-    }
-})
+//     }
+// })
 
-const me = new User({
-   name :'Aizah',
-   email:'acd@me.COM   '
+// const me = new User({
+//    name :'Aizah',
+//    email:'acd@me.COM   ',
+//    password: 'phone098'
     
-})
+// })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
-//  const Tasks = mongoose.model('Tasks', {
-//      discription:{
-//          type: String
-//      },
-//      completed:{
-//          type:Boolean
-//      }
-//  })
+ const Tasks = mongoose.model('Tasks', {
+     discription:{
+         type: String,
+         required: true,
+         trim : true,
+         
+     },
+     completed:{
+         type:Boolean,
+         default: false
+     }
+ })
 
-//  const first = new Tasks({
-//      discription:'Cleaning my room',
-//      completed: false
-//  })
+ const first = new Tasks({
+     discription:'Cleaning my room',
+     
+ })
 
-//  const second = new Tasks({
-//      discription:'Taking care of my family',
-//      completed: true
-//  })
 
-//  first.save().then(() => {
-//     console.log(first)
-//  }).catch((error) => {
-//      console.log('Error!' , error)
-//  })
-
-//  second.save().then(() => {
-//      console.log(second)
-//  }).catch((error) => {
-//      console.log('Error!' , error)
-//  })
+ first.save().then(() => {
+    console.log(first)
+ }).catch((error) => {
+     console.log('Error!' , error)
+ })
