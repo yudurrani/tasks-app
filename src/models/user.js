82da74343e239acaeae370ef027ce69b2814f96e
Yasir Durrani <yudurrani@gmail.com>
2019-08-13 10:58:33 -0400

    creating avatar and images

--- src/models/user.js
+++ src/models/user.js
@@ -48,7 +48,11 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+    
+    avatar:{
+        type: Buffer
+    }
     },{
     timestamps: true
 })
