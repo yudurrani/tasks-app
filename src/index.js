7526c812fc81d0e4c7a284e0408229c88251f641
Yasir Durrani <yudurrani@gmail.com>
2019-08-12 21:12:42 -0400

    starting file uploads

--- src/index.js
+++ src/index.js
@@ -8,18 +8,13 @@ const port = process.env.PORT || 3000
 const surveyRouter = require('./routers/survey')
 
 
-// app.use((req, res, next) =>{
-//    if(req.method === 'GET'){
-//     res.send('GET requests are disabled')
-//    }else{
-//        next()
-//    }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('The web is under maintenance')
-// })
-
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload',upload.single('upload'), (req, res) =>{
+    res.send()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -37,39 +32,9 @@ const User = require('./models/user')
 
 
 const main = async() => {
-    //const task = await Task.findById('5d4bb03b9daba3104148a854')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('5d4a0b9c7bd979729731d08f')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
+ 
 
 }
 
 main()
-
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {
-
-//     const token = jwt.sign({ _id:'abc123'}, 'thisismynewcourse', {expiresIn: '7 days'})
-//     console.log(token)
-
-
-//     const data = jwt.verify(token, 'thisismynewcourse')
-    
-//     console.log(data)
-
-    // const password = 'Red12345!'
-    // const hashedPassword = await bcrypt.hash(password, 8)
-
-    // console.log(password)
-    // console.log(hashedPassword)
-
-        // const isMatch = await bcrypt.compare('Red12345!', hashedPassword)
-        // console.log(isMatch)
-// }
-
-// myFunction()
 
--- src/routers/task.js
+++ src/routers/task.js
@@ -27,14 +27,20 @@ router.post('/tasks',auth, async(req, res) => {
 })
 
 // GET /tasks?completed=true
-
 // GET  /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt_desc (or:asc)
 
 router.get('/tasks', auth, async(req, res) => {
     const match = {}
+    const sort = {}
    
     if(req.query.completed){
         match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try{
@@ -44,7 +50,8 @@ router.get('/tasks', auth, async(req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
         res.send(req.user.tasks)
