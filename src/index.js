dcf5ecc817e5d866c81eaec83cd860caa45a12b6
Yasir Durrani <yudurrani@gmail.com>
2019-08-13 19:29:05 -0400

    cropping and image formatting

--- src/index.js
+++ src/index.js
@@ -8,30 +8,6 @@ const port = process.env.PORT || 3000
 const surveyRouter = require('./routers/survey')
 
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits:{
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb){
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return cb(new Error('Please upload a Word Document'))
-        }
-
-        cb(undefined, true)
-
-    }
-})
-
-// const errorMiddleware = (req, res, next) => {
-//     throw new Error('From my middleware ')
-// }
-app.post('/upload', upload.single('upload'), (req, res) =>{
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({error: error.message})
-})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const multer = require('multer')
+const sharp = require('sharp')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
@@ -122,7 +123,8 @@ const upload = multer({
     }
 })
 router.post('/users/me/avatar',auth, upload.single('avatar'), async(req, res) =>{
-  req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer()
+    req.user.avatar = buffer
    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -145,7 +147,7 @@ router.delete('/users/me/avatar',auth, async (req, res) => {
                 throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
 
      } catch(e){
