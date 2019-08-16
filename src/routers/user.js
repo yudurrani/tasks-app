b5319ef798b59b5b031a260b1f0e2353a1f1a41d
Yasir Durrani <yudurrani@gmail.com>
2019-08-15 20:38:24 -0400

    sending emails using sendgrid

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,8 @@ const User = require('../models/user')
 const multer = require('multer')
 const sharp = require('sharp')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelEmail } = require('../emails/account')
+
 const router = new express.Router()
 
 
@@ -11,7 +13,7 @@ router.post('/users', async(req, res) => {
 
     try {
         await user.save()
-       
+       sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
 
         res.status(201).send({user, token})
@@ -103,7 +105,9 @@ router.delete('/users/me', auth, async (req, res) => {
     try {
 
         await req.user.remove()
+        sendCancelEmail(req.user.email, req.user.name)
         res.send(req.user)
+        
     } catch(e){
         res.status(500).send(e)
     }
